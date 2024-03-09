5121faf91ce9b84b0aa094213993f07dd1074bc0
Sebastian Miozga <miozga.sebastian@gmail.com>
2024-03-09 09:35:28 +0100

    Update add-to-queue.js

---
+++ src/js/add-to-queue.js
@@ -0,0 +1,18 @@
+// Dodawanie filmów do kolejki
+const addToQueue = movieId => {
+  const queue = JSON.parse(localStorage.getItem('queueMovies')) || [];
+  if (!queue.includes(movieId)) {
+    queue.push(movieId);
+    localStorage.setItem('queueMovies', JSON.stringify(queue));
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const addToQueueBtn = document.getElementById('add-to-queue');
+  if (addToQueueBtn) {
+    addToQueueBtn.addEventListener('click', () => {
+      const movieId = addToQueueBtn.getAttribute('data-movie-id');
+      addToQueue(movieId);
+    });
+  }
+});
