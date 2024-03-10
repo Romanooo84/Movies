c61f691e376cc08529dde0f39b46d5b97c83303c
Pisarski Roman <romanooo84@gmail.com>
2024-03-10 09:47:39 +0100

    404

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './js/fetchMovies.js';
 import './js/moviesContainer.js';
-import './js/add-to-queue.js';
+import './js/add-to-queue.js'; // ymczasowy błąd 404
 import './js/watched.js';
 import './js/fetchKeyWords';
 import './sass/main.scss';
